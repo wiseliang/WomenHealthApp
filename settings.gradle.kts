d5f0532099786edc1aca7d897c21761a3a13a482
Administrator <80694095@qq.com>
2026-05-30 01:16:53 +0800

    Add Aliyun/Tencent mirrors for faster Gradle downloads in China

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,9 @@
 pluginManagement {
     repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -9,6 +13,9 @@ pluginManagement {
 
 dependencyResolution {
     repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
         google()
         mavenCentral()
         maven { url = uri("https://developer.huawei.com/repo/") }
